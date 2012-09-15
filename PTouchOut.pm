d6d84310c27bd70394d56628286b1809ffd85acf
John Meacham <john@repetae.net>
2012-09-14 22:03:05 -0400

    a
    Ignore-this: b512e1671421067dc1f35c09043d9bbd
    
    darcs-hash:20120915020305-1a7c6-d1530eeadc3d7d3902008e850b0b8b085942ed99

--- PTouchOut.pm
+++ PTouchOut.pm
@@ -1,11 +1,18 @@
 package PTouchOut;
 use strict;
 use warnings;
-use fields  qw/width output margin force/;
+use fields  qw/width output margin force minquality scale datamatrix/;
 use PTouch;
+use GdUtil qw/:all/;
 use v5.10;
 
 my PTouchOut $gself;
+
+my %default_values = (
+    output => "label.png",
+    margin => 2,
+    minquality => 'L',
+);
 
 sub self {
     my $self = shift;
@@ -18,7 +25,17 @@ sub opts {
     my ($self) = self(@_);
     return (
     "w=n" => \$self->{width},  # width of tape
-    "o=s" => \$self->{output}
+    "o=s" => \$self->{output},
+    );
+}
+
+sub opts_code {
+    my ($self) = self(@_);
+    return (
+    "M=n" => \$self->{margin},
+    "s=f" => \$self->{scale},
+    "q=s" => \$self->{minquality},
+    "force" => \$self->{force},
     );
 }
 
@@ -29,15 +46,33 @@ sub pixels {
 
 sub output {
     my ($self,$canvas) = self(@_);
-    use GdUtil;
-    GdUtil::writepng($canvas, $self->{output});
+    writepng($canvas, $self->{output});
 }
 
 sub new {
     my $self = shift;
     $self = fields::new($self) unless ref $self;
-    $self->{output} = "label.png";
+    %$self = %default_values;
+    hspace(1)->useFontConfig(1);
     return $self;
+}
+
+sub code {
+    my ($self,$text) = self(@_);
+    my $code = $self->{datamatrix} ? createcode_dm($text) : createcode_qr($text, quality => $self->{minquality});
+    my ($cbw,$cbh) = $code->getBounds();
+    $code = crop_centered($code,$cbw + 2*max(4,$self->{margin}) - 8, $cbh + 2*$self->{margin} - 8);
+    printf "modified QRBounds (%i,%i) -> (%i,%i)\n", $cbw, $cbh, $code->getBounds();
+
+    my ($cw,$ch) = $code->getBounds();
+    my $scale = $self->{scale};
+    if (!defined $scale) {
+        my $factor = $self->pixels / $ch;
+        $scale = $factor < 3.5 ? int($factor) : $factor;
+        printf "Scale factor: %.4f -> %.4f\n",$factor, $scale;
+        die "computed scale is too small to create readable code." if $scale < 2 && !$self->{force};
+    }
+    return stretch($code,$scale);
 }
 
 1;
